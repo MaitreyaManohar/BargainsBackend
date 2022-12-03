65737b7c179b17f16665c1b8191f07f4e8939848
MaitreyaManohar <103531625+MaitreyaManohar@users.noreply.github.com>
2022-12-03 18:51:57 +0530

    AddedCrossOrigin

--- src/main/java/com/example/oopsProject/Mail/EmailController.java
+++ src/main/java/com/example/oopsProject/Mail/EmailController.java
@@ -5,12 +5,10 @@ import com.example.oopsProject.Security.passwordencoder;
 import com.example.oopsProject.UserClass.UserClass;
 import com.example.oopsProject.UserClass.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("mail")
 public class EmailController {
     private EmailService emailService;
